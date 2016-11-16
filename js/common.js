4edf0a654b228400cfeb5678d7c078b35e6cb9e0
Ikles <membship@ya.ru>
2016-11-16 14:17:32 +0800

    commit

--- js/common.js
+++ js/common.js
@@ -1,8 +1,18 @@
-$(function() {
+$(document).ready(function(){
 
 	$(".toggle-mnu").click(function() {
 		$(this).toggleClass("on");
 		$(".main-mnu").slideToggle();
 		return false;
 	});
-});
+
+  //$('#header').after($("#bukkit"));
+
+
+
+
+
+
+
+
+}); //end ready
